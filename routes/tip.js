529b817eac890f3c8670bb18c07c69243b4e3def
chike onuorah <chikeonuorah@chikeonorahsMBP.lan>
2019-03-10 16:41:15 -0700

    add comments to tip routes

--- routes/tip.js
+++ routes/tip.js
@@ -7,44 +7,11 @@ const
 
     router.get('/', tipControllers.findAllTips);
     router.get('/:city', tipControllers.findTipsByCity);
-<<<<<<< HEAD
     
     // protected routes
     router.post('/', verifyToken ,tipControllers.postTip);
     router.delete('/:tipId', verifyToken, tipControllers.deleteTip);
     router.put('/:data', verifyToken, tipControllers.updateTip);
     
-=======
-
-    router.post('/', tipControllers.postTip);
-    router.delete('/:tipId', tipControllers.deleteTip);
-    router.put('/:data', tipControllers.updateTip);
-
-
-
-
-
-    router.use((req, res, next) => {
-        console.log('activated')
-        const bearerHeader = req.headers['authorization'];
-        console.log('triggered token check', bearerHeader)
-    
-        if (typeof bearerHeader !== 'undefined') {
-            const bearer = bearerHeader.split(' ');
-            const bearerToken = bearer[1];
-            req.token = bearerToken;
-            let verified = jwt.verify(req.token, 'cantaloupe');
-            console.log('here is the verified', verified)
-            req.userId = verified._id
-            next(); 
-        } else {
-            res.sendStatus(403);
-        }
-    })
-
-
- 
-
->>>>>>> 5002273a1b9d9b48ad4cabb953821af833e8d760
 
 module.exports = router;
